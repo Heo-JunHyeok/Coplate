e8c0ef9c59ccf9fee98cddb504cced9a40d9cffb
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-18 16:07:27 +0900

    review model, media files

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# MEDIA FILES
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/uploads/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
