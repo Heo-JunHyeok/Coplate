0cd6c5fec96073043847d8c5132e5a9b9b3a0027
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-25 22:44:39 +0900

    프로필 페이지 구현하기

--- coplate/views.py
+++ coplate/views.py
@@ -11,7 +11,7 @@ from braces.views import LoginRequiredMixin, UserPassesTestMixin
 from allauth.account.models import EmailAddress
 from allauth.account.views import PasswordChangeView
 from .forms import ReviewForm
-from .models import Review
+from .models import Review, User
 from .functions import confirmation_required_redirect
 
 
@@ -79,6 +79,23 @@ class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self, user):
         review = self.get_object()
         return review.author == user
+
+
+class ProfileView(DetailView):
+    model = User
+    template_name = "coplate/profile.html"
+    pk_url_kwarg = "user_id"
+    context_object_name = "profile_user"
+    # default 값인 user는 현재 유저를 참조하는 것으로 사용했기 때문에
+    # 충돌하는 것을 방지하기 위해 context_object_name 사용
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_id = self.kwargs.get("user_id")
+        context["user_reviews"] = Review.objects.filter(author__id=user_id).order_by(
+            "-dt_created"
+        )[:4]
+        return context
 
 
 class CustomPasswordChangeView(PasswordChangeView):
