3be892afa32ec34b905c21ff658c15a52cdc649d
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-27 16:06:01 +0900

    middleware

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "coplate.middleware.ProfileSetupMiddleware",  # request.user를 사용하기 때문에 AuthenticationMiddleware 뒤에 위치
 ]
 
 ROOT_URLCONF = "config.urls"
